a28d9d8200d229d797c416d784dd3381b7de425c
Osvald <79842922+Ozzuke@users.noreply.github.com>
2022-03-23 23:15:58 +0200

    commit to github

--- boar.py
+++ boar.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
+# A program to view, add, edit and export short references for later use (CLI apps, commands, websites, books etc)
+# Author Osvald Nigola
 
-print("Hello world!")
+
+import argparse  # module for parsing arguments passed from the command line
+
+
