98f1a3beab153671c68801da9ed3ffc24b09e476
Osvald <79842922+Ozzuke@users.noreply.github.com>
2022-03-30 22:54:09 +0300

    creating of default items in freshly created directory, cleanup

--- boar.py
+++ boar.py
@@ -12,6 +12,7 @@ import time
 
 def create_data_dir(local_path_normal, local_path_config):
     """Create the directory for storing the application's files"""
+    
     # prompt to create the directory
     for i in range(5):
         do_setup = input("It seems no data directory has been set up yet. Proceed? [Y/n] ")
@@ -22,10 +23,9 @@ def create_data_dir(local_path_normal, local_path_config):
         if i != 4:
             time.sleep(0.5)
             continue
-        print("Unable to understand.")
-        exit()
+        exit("Unable to understand.")
     
-    # prompt about the location to create the directory in
+    # prompt about the location to create the directory in, then create it
     for i in range(5):
         dir_where = input(f"\nWhere to create the directory? \n1: {local_path_normal}  (default) \n2: {local_path_config} \nQ to abort \nSelect location [1-2] (default 1) ")
         if dir_where in ["1", ""]:
@@ -44,33 +44,11 @@ def create_data_dir(local_path_normal, local_path_config):
         if i != 4:
             time.sleep(0.5)
             continue
-        print("Unable to understand.")
-        exit()
+        exit("Unable to understand.")
 
 
-def main():
-    
-    # create partser to parse arguments passed from the command line
-    parser = argparse.ArgumentParser(description="Add, edit and view a list of short references.")
-    parser.add_argument("foo", nargs="*")  # argument to gather all input from the command line into a list
-    args = parser.parse_args().foo  # a list of all non-positional input
-    
-    # exit if no arguments were passed
-    #TODO: possibly change to same as ls
-    if not args:
-        print("No arguments passed.")
-        exit()
-    
-    # check if passed argument for action is a valid one
-    if args[0] in ["add", "addcat", "ls", "lscat", "rm", "rmcat", "reset", "undo", "export"]:
-        act = args.pop(0)
-    else:
-        print("Invalid argument.")
-        exit()
-    
-    # path to data directory
-    path = os.path.expanduser("~") + "/.boar/"
-    path2 = os.path.expanduser("~") + "/.config/boar/"
+def create_defaults(path_loc):
+    """Create the contents to the data folder"""
     
     # the main data structure for keeping the references
     book = [
@@ -97,12 +75,42 @@ def main():
         "history length": 5,
         "disable colors": False
     }
+
+    # create the file to store the book data in with the template category
+    with open(path_loc + "book", "w") as file:
+        file.write(json.dumps(book))
+    # create the config file
+    with open(path_loc + "conf", "w") as file:
+        file.write(json.dumps(conf))
+    # create the directory to keep the last n copies of the book in for ability to undo actions
+    os.mkdir(path_loc + "history")
+        
+
+def main():
     
-    # check if data directory exists and handle appropriately
-    if os.path.exists(path) or os.path.exists(path2):
-        pass
+    # create partser to parse arguments passed from the command line
+    parser = argparse.ArgumentParser(description="Add, edit and view a list of short references.")
+    parser.add_argument("foo", nargs="*")  # argument to gather all input from the command line into a list
+    args = parser.parse_args().foo  # a list of all non-positional input
+    
+    # check if passed argument for action is a valid one and store it
+    if not args:
+        act = "ls"
+    elif args[0] in ["add", "addcat", "ls", "rm", "rmcat", "reset", "undo", "export"]:
+        act = args.pop(0)
     else:
+        exit("Invalid argument.")
+    
+    # path to data directory
+    path = os.path.expanduser("~") + "/.boar/"
+    path2 = os.path.expanduser("~") + "/.config/boar/"
+    
+    # check if data directory exists and handle appropriately if it doesn't
+    if not (os.path.exists(path) or os.path.exists(path2)):
         create_data_dir(path, path2)
+        path = path if os.path.exists(path) else path2
+        create_defaults(path)
+    path = path if os.path.exists(path) else path2
 
 
 if __name__ == "__main__":
