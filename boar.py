7ec3ee3d5e1342e0b90c5e0bf9dfb617e1123404
Osvald <79842922+Ozzuke@users.noreply.github.com>
2022-03-30 21:38:02 +0300

    creation of data folder

--- boar.py
+++ boar.py
@@ -15,21 +15,87 @@ def boar():
     parser.add_argument("foo", nargs="*")  # argument to gather all input from the command line into a list
     args = parser.parse_args().foo  # a list of all non-positional input
     
+    # exit if no arguments were passed
+    #TODO: possibly change to same as ls
     if not args:
         print("No arguments passed.")
         exit()
     
-    if args[0] in ["add", "addcat", "ls", "lscat", "rm", "rmcat", "export"]:
+    # check if passed argument for action is a valid one
+    if args[0] in ["add", "addcat", "ls", "lscat", "rm", "rmcat", "reset", "undo", "export"]:
         act = args.pop(0)
     else:
         print("Invalid argument.")
         exit()
     
+    # path to data directory
     path = os.path.expanduser("~") + "/.boar/"
+    path2 = os.path.expanduser("~") + "/.config/boar/"
     
-    with open(os.path.expanduser("~") + "/.vimrc") as testfail:
-        print(testfail.read())
+    # the main data structure for keeping the references
+    book = [
+        {
+            "name": "Template Category",
+            "short": "temp",
+            "items": [
+                {
+                    "name": "Template entry 1",
+                    "desc": "A good description about the entry",
+                    "link": "https://example.com"
+                },
+                {
+                    "name": "A second template entry",
+                    "desc": None,
+                    "link": None
+                }
+            ],
+        }
+    ]
     
+    # configuration
+    conf = {
+        "history length": 5
+    }
+    
+    # check if data directory exists and handle appropriately
+    if os.path.exists(path) or os.path.exists(path2):
+        pass
+    else:
+        # prompt to create the directory
+        for i in range(5):
+            do_setup = input("It seems no data directory has been set up yet. Proceed? [Y/n] ")
+            if do_setup.lower() in ["y", "yes", ""]:
+                break
+            elif do_setup.lower() in ["n", "no"]:
+                exit()
+            if i != 4:
+                continue
+            print("Unable to understand.")
+            exit()
+        
+        # prompt about the location to create the directory in
+        for i in range(5):
+            dir_where = input(f"Where to create the directory? \n1: {path} (default) \n2: {path2} \nSelect location [1-2] (default 1) ")
+            if dir_where in ["1", ""]:
+                os.mkdir(path)
+                print("Created directory", path)
+                break
+            elif dir_where in ["2"]:
+                try:
+                    os.mkdir(path2)
+                    print("Created directory", path2)
+                    break
+                except FileNotFoundError:
+                    print(f"The directory '{os.path.expanduser('~')}/.config/' was not found.")
+            elif dir_where.lower() in ["q", "n", "no"]:
+                exit()
+            if i != 4:
+                continue
+            print("Unable to understand.")
+            exit()
+        
+        print("created dir")
+
 
 if __name__ == "__main__":
     boar()
