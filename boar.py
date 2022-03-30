e89638c034781d8db39fdc3df54b0e618f6edc6a
Osvald <79842922+Ozzuke@users.noreply.github.com>
2022-03-30 22:12:04 +0300

    moved folder creation from main function to a separate one

--- boar.py
+++ boar.py
@@ -7,9 +7,49 @@
 import argparse  # module for parsing arguments passed from the command line
 import os
 import json
+import time
 
 
-def boar():
+def create_data_dir(local_path_normal, local_path_config):
+    """Create the directory for storing the application's files"""
+    # prompt to create the directory
+    for i in range(5):
+        do_setup = input("It seems no data directory has been set up yet. Proceed? [Y/n] ")
+        if do_setup.lower() in ["y", "yes", ""]:
+            break
+        elif do_setup.lower() in ["n", "no"]:
+            exit()
+        if i != 4:
+            time.sleep(0.5)
+            continue
+        print("Unable to understand.")
+        exit()
+    
+    # prompt about the location to create the directory in
+    for i in range(5):
+        dir_where = input(f"\nWhere to create the directory? \n1: {local_path_normal}  (default) \n2: {local_path_config} \nQ to abort \nSelect location [1-2] (default 1) ")
+        if dir_where in ["1", ""]:
+            os.mkdir(local_path_normal)
+            print("Created directory at", local_path_normal)
+            break
+        elif dir_where in ["2"]:
+            try:
+                os.mkdir(local_path_config)
+                print("Created directory at", local_path_config)
+                break
+            except FileNotFoundError:
+                print(f"The directory '{os.path.expanduser('~')}/.config/' was not found.")
+        elif dir_where.lower() in ["q", "quit", "a", "abort", "n", "no"]:
+            exit()
+        if i != 4:
+            time.sleep(0.5)
+            continue
+        print("Unable to understand.")
+        exit()
+
+
+def main():
+    
     # create partser to parse arguments passed from the command line
     parser = argparse.ArgumentParser(description="Add, edit and view a list of short references.")
     parser.add_argument("foo", nargs="*")  # argument to gather all input from the command line into a list
@@ -54,48 +94,16 @@ def boar():
     
     # configuration
     conf = {
-        "history length": 5
+        "history length": 5,
+        "disable colors": False
     }
     
     # check if data directory exists and handle appropriately
     if os.path.exists(path) or os.path.exists(path2):
         pass
     else:
-        # prompt to create the directory
-        for i in range(5):
-            do_setup = input("It seems no data directory has been set up yet. Proceed? [Y/n] ")
-            if do_setup.lower() in ["y", "yes", ""]:
-                break
-            elif do_setup.lower() in ["n", "no"]:
-                exit()
-            if i != 4:
-                continue
-            print("Unable to understand.")
-            exit()
-        
-        # prompt about the location to create the directory in
-        for i in range(5):
-            dir_where = input(f"Where to create the directory? \n1: {path} (default) \n2: {path2} \nSelect location [1-2] (default 1) ")
-            if dir_where in ["1", ""]:
-                os.mkdir(path)
-                print("Created directory", path)
-                break
-            elif dir_where in ["2"]:
-                try:
-                    os.mkdir(path2)
-                    print("Created directory", path2)
-                    break
-                except FileNotFoundError:
-                    print(f"The directory '{os.path.expanduser('~')}/.config/' was not found.")
-            elif dir_where.lower() in ["q", "n", "no"]:
-                exit()
-            if i != 4:
-                continue
-            print("Unable to understand.")
-            exit()
-        
-        print("created dir")
+        create_data_dir(path, path2)
 
 
 if __name__ == "__main__":
-    boar()
+    main()
