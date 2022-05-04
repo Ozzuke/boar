e89377eef4d3241682b1ad6f888c75e674c835d4
Osvald <79842922+Ozzuke@users.noreply.github.com>
2022-05-04 18:43:29 +0300

    remove author name

--- boar.py
+++ boar.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 # A program to view, add, edit and export short references for later use (CLI apps, commands, websites, books etc)
-# Author Osvald Nigola
 
 import argparse  # module for parsing arguments passed from the command line
 import os
